452e50150a5f331afb6fe8eae35d0797eb7234b8
ririnhandayani12 <rhandayani1211@gmail.com>
2021-11-22 14:28:04 +0700

    pertemuan 9

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -3,85 +3,88 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class User extends CI_Controller
 {
-  public function __construct()
-  {
-    parent::__construct();
-    cek_login();
-  }
+    public function __construct()
+    {
+        parent::__construct();
+        cek_login();
+    }
 
-  public function index()
-  {
-    $data['judul'] = 'Profile Saya';
-    $data['user'] = $this->ModelUser->cekData(['email' => $this->session->userdata('email')])->row_array();
+    public function index()
+    {
+        $data['judul'] = 'Profil Saya';
+        $data['user'] = $this->ModelUser->cekData(['email' => $this->session->userdata('email')])->row_array();
 
-    $this->load->view('templates/header', $data);
-    $this->load->view('templates/sidebar', $data);
-    $this->load->view('templates/topbar', $data);
-    $this->load->view('user/index', $data);
-    $this->load->view('templates/footer');
-  }
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('user/index', $data);
+        $this->load->view('templates/footer');
+    }
 
-  public function anggota()
-  {
-    $data['judul'] = 'Data Anggota';
-    $data['user'] = $this->ModelUser->cekData(['email' => $this->session->userdata('email')])->row_array();
-    $this->db->where('role_id', 1);
-    $data['anggota'] = $this->db->get('user')->result_array();
+    public function anggota()
+    {
+        $data['judul'] = 'Data Anggota';
+        $data['user'] = $this->ModelUser->cekData(['email' => $this->session->userdata('email')])->row_array();
+        $this->db->where('role_id', 1);
+        $data['anggota'] = $this->db->get('user')->result_array();
 
-    $this->load->view('templates/header', $data);
-    $this->load->view('templates/sidebar', $data);
-    $this->load->view('templates/topbar', $data);
-    $this->load->view('user/anggota', $data);
-    $this->load->view('templates/footer');
-  }
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('user/anggota', $data);
+        $this->load->view('templates/footer');
+    }
 
-  public function ubahProfile()
-  {
-    $data['judul'] = 'Ubah Profile';
-    $data['user'] = $this->ModelUser->cekData(['email' => $this->session->userdata('email')])->row_array();
+    public function ubahProfil()
+    {
+        $data['judul'] = 'Ubah Profil';
+        $data['user'] = $this->ModelUser->cekData(['email' => $this->session->userdata('email')])->row_array();
 
-    $this->form_validation->set_rules('nama', 'Nama Lengkap', 'required|trim', [
-      'required' => 'Nama Tidak Boleh Kosong'
-    ]);
-    if($this->form_validation->run() == false){
-      $this->load->view('templates/header', $data);
-      $this->load->view('templates/sidebar', $data);
-      $this->load->view('templates/topbar', $data);
-      $this->load->view('user/ubah-profile', $data);
-      $this->load->view('templates/footer');
-    }else{
-      $nama = $this->input->post('nama', true);
-      $email = $this->input->post('email', true);
+        $this->form_validation->set_rules('nama', 'Nama Lengkap', 'required|trim', [
+            'required' => 'Nama tidak Boleh Kosong'
+        ]);
 
-      //jika ada gambar akan diupload
-      $upload_image = $_FILES['image']['name'];
 
-      if($upload_image){
-        $config['upload_path'] = './assets/img/profile/';
-        $config['allowed_types'] = 'gif|jpg|png';
-        $config['max_size'] = '3000';
-        $config['max_width'] = '1024';
-        $config['max_height'] = '1000';
-        $config['file_name'] = 'pro' . time();
+        if ($this->form_validation->run() == false) {
+            $this->load->view('templates/header', $data);
+            $this->load->view('templates/sidebar', $data);
+            $this->load->view('templates/topbar', $data);
+            $this->load->view('user/ubah-profile', $data);
+            $this->load->view('templates/footer');
+        } else {
+            $nama = $this->input->post('nama', true);
+            $email = $this->input->post('email', true);
 
-        $this->load->library('upload', $config);
+            //jika ada gambar yang akan diupload
+            $upload_image = $_FILES['image']['name'];
 
-        if($this->upload->do_upload('image')){
-          $gambar_lama = $data['user']['image'];
-          if($gambar_lama != 'default.jpg'){
-            unlink(FCPATH . 'assets/img/profile/' . $gambar_lama);
-          }
-          $gambar_baru = $this->upload->data('file_name');
-          $this->db->set('image', $gambar_baru);
-        }else{ }
-      }
+            if ($upload_image) {
+                $config['upload_path'] = './assets/img/profile/';
+                $config['allowed_types'] = 'gif|jpg|png';
+                $config['max_size']     = '3000';
+                $config['max_width'] = '1024';
+                $config['max_height'] = '1000';
+                $config['file_name'] = 'pro' . time();
 
-      $this->db->set('nama', $nama);
-      $this->db->where('email', $email);
-      $this->db->update('user');
+                $this->load->library('upload', $config);
 
-      $this->session->set_flashdata('pesan', '<div class="alert alert-success alert-message" role="alert">Profile Berhasil Diubah</div>');
-      redirect('user');
+                if ($this->upload->do_upload('image')) {
+                    $gambar_lama = $data['user']['image'];
+                    if ($gambar_lama != 'default.jpg') {
+                        unlink(FCPATH . 'assets/img/profile/' . $gambar_lama);
+                    }
+
+                    $gambar_baru = $this->upload->data('file_name');
+                    $this->db->set('image', $gambar_baru);
+                } else { }
+            }
+
+            $this->db->set('nama', $nama);
+            $this->db->where('email', $email);
+            $this->db->update('user');
+
+            $this->session->set_flashdata('pesan', '<div class="alert alert-success alert-message" role="alert">Profil Berhasil diubah </div>');
+            redirect('user');
+        }
     }
   }
-}
--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -1,7 +1,7 @@
 <!-- Begin Page Content -->
 <div class="container-fluid">
 
-  <!-- row ux -->
+  <!-- row ux-->
   <div class="row">
     <div class="col-xl-3 col-md-6 mb-4">
       <div class="card border-left-danger shadow h-100 py-2 bg-primary">
@@ -9,14 +9,11 @@
           <div class="row no-gutters align-items-center">
             <div class="col mr-2">
               <div class="text-md font-weight-bold text-white text-uppercase mb-1">Jumlah Anggota</div>
-              <div class="h1 mb-0 font-weight-bold text-white"><?= $this->ModelUser->getUserWhere(['role_id' => 1])->num_rows(); ?>
-              </div>
+              <div class="h1 mb-0 font-weight-bold text-white"><?= $this->ModelUser->getUserWhere(['role_id' => 1])->num_rows(); ?></div>
             </div>
-              <div class="col-auto">
-                <a href="<?= base_url('user/anggota'); ?>">
-                  <i class="fas fa-users fa-3x text-warning"></i>
-                </a>
-              </div>
+            <div class="col-auto">
+              <a href="<?= base_url('user/anggota'); ?>"><i class="fas fa-users fa-3x text-warning"></i></a>
+            </div>
           </div>
         </div>
       </div>
@@ -27,20 +24,17 @@
         <div class="card-body">
           <div class="row no-gutters align-items-center">
             <div class="col mr-2">
-              <div class="text-md font-weight-bold text-white text-uppercase mb-1">Stok Buku Terdaftar
-              </div>
+              <div class="text-md font-weight-bold text-white text-uppercase mb-1">Stok Buku Terdaftar</div>
               <div class="h1 mb-0 font-weight-bold text-white">
-                <?php 
-                  $where = ['stok != 0'];
-                  $totalstok = $this->ModelBuku->total('stok', $where);
-                  echo $totalstok;                
+                <?php
+                $where = ['stok != 0'];
+                $totalstok = $this->ModelBuku->total('stok', $where);
+                echo $totalstok;
                 ?>
               </div>
             </div>
             <div class="col-auto">
-              <a href="<?= base_url('buku'); ?>">
-                <i class="fas fa-book fa-3x text-primary"></i>
-              </a>
+              <a href="<?= base_url('buku'); ?>"><i class="fas fa-book fa-3x text-primary"></i></a>
             </div>
           </div>
         </div>
@@ -52,20 +46,17 @@
         <div class="card-body">
           <div class="row no-gutters align-items-center">
             <div class="col mr-2">
-              <div class="text-md font-weight-bold text-white text-uppercase mb-1">Buku yang dipinjam
-              </div>
+              <div class="text-md font-weight-bold text-white text-uppercase mb-1">Buku yang dipinjam</div>
               <div class="h1 mb-0 font-weight-bold text-white">
                 <?php
-                  $where = ['dipinjam != 0'];
-                  $totaldipinjam = $this->ModelBuku->total('dipinjam', $where);
-                  echo $totaldipinjam;
+                $where = ['dipinjam != 0'];
+                $totaldipinjam = $this->ModelBuku->total('dipinjam', $where);
+                echo $totaldipinjam;
                 ?>
               </div>
             </div>
             <div class="col-auto">
-              <a href="<?= base_url('user'); ?>">
-                <i class="fas fa-user-tag fa-3x text-success"></i>
-              </a>
+              <a href="<?= base_url('user'); ?>"><i class="fas fa-user-tag fa-3x text-success"></i></a>
             </div>
           </div>
         </div>
@@ -77,39 +68,34 @@
         <div class="card-body">
           <div class="row no-gutters align-items-center">
             <div class="col mr-2">
-              <div class="text-md font-weight-bold text-white text-uppercase mb-1">Buku yang dibooking
-              </div>
+              <div class="text-md font-weight-bold text-white text-uppercase mb-1">Buku yang dibooking</div>
               <div class="h1 mb-0 font-weight-bold text-white">
                 <?php
-                  $where = ['dibooking != 0'];
-                  $totaldibooking = $this->ModelBuku->total('dibooking', $where);
-                  echo $totaldibooking;
+                $where = ['dibooking !=0'];
+                $totaldibooking = $this->ModelBuku->total('dibooking', $where);
+                echo $totaldibooking;
                 ?>
               </div>
             </div>
-            <div class="col-aut">
-              <a href="<?= base_url('user'); ?>">
-                <i class="fas fa-shopping-cart fa-3x text-danger"></i>
-              </a>
+            <div class="col-auto">
+              <a href="<?= base_url('user'); ?>"><i class="fas fa-shopping-cart fa-3x text-danger"></i></a>
             </div>
           </div>
         </div>
       </div>
     </div>
-</div>
-  <!-- End row ux -->
+  </div>
+  <!-- end row ux-->
 
   <!-- Divider -->
-  <hr class="sidebar-divid">
+  <hr class="sidebar-divider">
 
-  <!-- row table -->
+  <!-- row table-->
   <div class="row">
     <div class="table-responsive table-bordered col-sm-5 ml-auto mr-auto mt-2">
       <div class="page-header">
-        <span class="fas fa-users text-primary mt-2">Data User</span>
-        <a href="<?= base_url('user/data_user'); ?>" class="text-danger">
-          <i class="fas fa-search mt-2 float-right"> Tampilkan</i>
-        </a>
+        <span class="fas fa-users text-primary mt-2 "> Data User</span>
+        <a class="text-danger" href="<?php echo base_url('user/data_user'); ?>"><i class="fas fa-search mt-2 float-right"> Tampilkan</i></a>
       </div>
       <table class="table mt-3">
         <thead>
@@ -125,7 +111,7 @@
         <tbody>
           <?php
           $i = 1;
-          foreach ($anggota as $a){ ?>
+          foreach ($anggota as $a) { ?>
             <tr>
               <td><?= $i++; ?></td>
               <td><?= $a['nama']; ?></td>
@@ -134,18 +120,16 @@
               <td><?= $a['is_active']; ?></td>
               <td><?= date('Y', $a['tanggal_input']); ?></td>
             </tr>
-          }
           <?php } ?>
         </tbody>
       </table>
     </div>
 
+
     <div class="table-responsive table-bordered col-sm-5 ml-auto mr-auto mt-2">
       <div class="page-header">
-        <span class="fas fa-book text-warning mt-2">Data Buku</span>
-        <a href="<?= base_url('buku'); ?>">
-          <i class="fas fa-search text-primary mt-2 float-right">Tampilkan</i>
-        </a>
+        <span class="fas fa-book text-warning mt-2"> Data Buku</span>
+        <a href="<?= base_url('buku'); ?>"><i class="fas fa-search text-primary mt-2 float-right"> Tampilkan </i></a>
       </div>
       <div class="table-responsive">
         <table class="table mt-3" id="table-datatable">
@@ -164,26 +148,27 @@
             <?php
             $i = 1;
             foreach ($buku as $b) { ?>
-            <tr>
-              <td><?= $i++; ?></td>
-              <td><?= $b['judul_buku']; ?></td>
-              <td><?= $b['pengarang']; ?></td>
-              <td><?= $b['penerbit']; ?></td>
-              <td><?= $b['tahun_terbit']; ?></td>
-              <td><?= $b['isbn']; ?></td>
-              <td><?= $b['stok']; ?></td>
-            </tr>
+              <tr>
+                <td><?= $i++; ?></td>
+                <td><?= $b['judul_buku']; ?></td>
+                <td><?= $b['pengarang']; ?></td>
+                <td><?= $b['penerbit']; ?></td>
+                <td><?= $b['tahun_terbit']; ?></td>
+                <td><?= $b['isbn']; ?></td>
+                <td><?= $b['stok']; ?></td>
+              </tr>
             <?php } ?>
           </tbody>
         </table>
       </div>
     </div>
 
-  </div>
-  <!-- End of row table -->
 
   </div>
-  <!-- container-fluid -->
+  <!-- end of row table-->
 
 </div>
-<!-- End of Main content -->
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
