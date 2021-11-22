fa6e4646a923f8b36bcc18a93ce9b9e2b1559aee
ririnhandayani12 <rhandayani1211@gmail.com>
2021-11-22 16:07:04 +0700

    pertemuan 9

--- application/models/ModelUser.php
+++ application/models/ModelUser.php
@@ -7,17 +7,14 @@ class ModelUser extends CI_Model
     {
         $this->db->insert('user', $data);
     }
-
     public function cekData($where = null)
     {
         return $this->db->get_where('user', $where);
     }
-
     public function getUserWhere($where = null)
     {
         return $this->db->get_where('user', $where);
     }
-
     public function cekUserAccess($where = null)
     {
         $this->db->select('*');
@@ -25,7 +22,6 @@ class ModelUser extends CI_Model
         $this->db->where($where);
         return $this->db->get();
     }
-
     public function getUserLimit()
     {
         $this->db->select('*');
@@ -33,5 +29,4 @@ class ModelUser extends CI_Model
         $this->db->limit(10, 0);
         return $this->db->get();
     }
-
 }
